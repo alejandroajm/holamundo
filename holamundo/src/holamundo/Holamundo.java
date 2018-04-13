ae5952b97a24869ea76418e2842e9d080d321f5f
pc <pc@pc-VirtualBox>
2018-04-13 16:20:50 +0200

    Version 1.2 hola mundo

--- holamundo/src/holamundo/Holamundo.java
+++ holamundo/src/holamundo/Holamundo.java
@@ -17,6 +17,7 @@ public class Holamundo {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("hola mundo");
+        System.out.print(".......");
         
     }
     
